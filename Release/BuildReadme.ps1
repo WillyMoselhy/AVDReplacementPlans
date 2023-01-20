9cd53c59de0d3a30fa7fabd75648968ef689bc59
Willy Moselhy [MSFT] <willy@moselhy.com>
2023-01-20 14:12:25 +0400

    Update parameters description and readme

--- Release/BuildReadme.ps1
+++ Release/BuildReadme.ps1
@@ -1,38 +1,64 @@
 $TemplateFilePath = '.\Build\Bicep\FunctionApps.bicep'
 
+#region: Helper Functions
+function ConvertTo-MarkdownTable {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [PSObject]
+        $InputObject
+    )
+
+    $header = $InputObject[0].PSObject.Properties.Name
+    $header = "| " + ($header -join " | " )+ " |"
+
+    $headerSeparator = $header -replace "[^\|]", "-"
+
+    $rows = foreach ($row in $InputObject) {
+        $row = $row.PSObject.Properties.Value
+        $row = "| " +( $row -join " | ") + " |"
+        $row
+    }
+
+    (@($header,$headerSeparator) + $rows) -join "`n"
+
+}
+
+#endregion: Helper Functions
+
+
 # Get the template file
 $templateFile = Get-Content -Path $TemplateFilePath
 
 # Extract @description lines
 $descriptionLines = $TemplateFile | Where-Object { $_ -like "@description(*" }
 
+# Remove the @description and )' from the end of each line and convert to CSV
+
+
 # for each description, get its index and extract info
 $templateParameters = foreach ($line in $descriptionLines) {
-    # get the location of this description
+    # Get index of line in template file
     $index = $TemplateFile.IndexOf($line)
 
-    # get parameter details
+    # Get the parameter name and type
+    $TemplateFile[$index + 1] -match "param\s+(?<paramName>\w+)\s+(?<paramType>\w+)" | Out-Null
+    $paramName = $Matches.paramName
+    $paramType = $Matches.paramType
 
-    if ($line -match "Required:\s*(?<Required>\w+)\s*\|\s*(?<Description>.+)(?:'\)|\s*\|\s*)Default:\s*(?<Default>.+)'\)") {
-        $paramRequired = $matches['Required']
-        $paramDescription = $matches['Description']
-    }
-    else{
-        Write-Error "Failed to extract parameter description details from line ($index): $line"
+    $descriptionCSV = $line
+    | ForEach-Object { $_.Substring(24) }
+    | ForEach-Object { $_.TrimEnd("')'") }
+    | ConvertFrom-Csv -Delimiter "|" -Header "Required", "Description", "Default"
+
+    [PSCustomObject]@{
+        Name        = $paramName
+        required    = $descriptionCSV.Required
+        Description = $descriptionCSV.Description
+        Type        = $paramType
+        Default     = if ($descriptionCSV.Default) { $descriptionCSV.Default.Substring(9) }
     }
 
-    $paramLine = $templateFile[$index + 1]
-    if ($paramLine -match '^param\s+(?<Name>\w+)\s+(?<Type>\w+)(?:\s*=\s*(?<DefaultValue>.+))?$') {
-        [PSCustomObject]@{
-            Name         = $matches['Name']
-            Type         = $matches['Type']
-            Required     = $paramRequired
-            Description  = $paramDescription
-            DefaultValue = $matches['DefaultValue']
-        }
-    }
-    else {
-        Write-Error "Failed to extract parameter details from line ($index): $line"
-    }
+
 }
-$templateParameters | ft
+ConvertTo-MarkdownTable -InputObject ($templateParameters | Sort-Object -Property Name )   | Set-Clipboard
