a058f5fd12a20a0e9ad5578356fb96d83856abef
Willy Moselhy [MSFT] <willy@moselhy.com>
2023-02-16 14:30:55 +0200

    Update Function version to 0.1.2

--- FunctionApp/profile.ps1
+++ FunctionApp/profile.ps1
@@ -27,4 +27,4 @@ else{
 $ErrorActionPreference = 'Stop'
 
 # Version Banner #
-Write-PSFMessage -Level Host -Message "This is SessionHostReplacer version {0}" -StringValues '0.1.1'
+Write-PSFMessage -Level Host -Message "This is SessionHostReplacer version {0}" -StringValues '0.1.2'
