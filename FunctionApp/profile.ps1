0663f7bc3aa28cc7f8baab7cb5adfb887a7d4691
Willy Moselhy [MSFT] <willy@moselhy.com>
2023-03-28 10:25:09 +0200

    Update Banner

--- FunctionApp/profile.ps1
+++ FunctionApp/profile.ps1
@@ -14,7 +14,7 @@ Import-Module 'PSFrameWork' ,'Az.Resources' ,'Az.Compute' ,'Az.DesktopVirtualiza
 # Configure PSFramework settings
 Set-PSFConfig -FullName PSFramework.Message.style.NoColor -Value $true #This is required for logs to look good in FunctionApp Logs
 
-# Version Banner #
+# Version Banner # Updated by Build\Build-Zip-File.ps1
 Write-PSFMessage -Level Host -Message "This is SessionHostReplacer version {0}" -StringValues '0.1.5'
 
 # Import Function Parameters
