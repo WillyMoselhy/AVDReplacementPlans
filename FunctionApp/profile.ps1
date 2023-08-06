6f282dab2d37b13b1675f5654f2836a4b08b9d2c
Willy Moselhy [MSFT] <willy@moselhy.com>
2023-08-06 12:47:57 +0300

    update version number [no-ci]

--- FunctionApp/profile.ps1
+++ FunctionApp/profile.ps1
@@ -15,7 +15,7 @@ Import-Module 'PSFrameWork' ,'Az.Resources' ,'Az.Compute' ,'Az.DesktopVirtualiza
 Set-PSFConfig -FullName PSFramework.Message.style.NoColor -Value $true #This is required for logs to look good in FunctionApp Logs
 
 ## Version Banner ## Updated by Build\Build-Zip-File.ps1
-Write-PSFMessage -Level Host -Message "This is SessionHostReplacer version {0}" -StringValues '20230806-075231 (Repo)'
+Write-PSFMessage -Level Host -Message "This is SessionHostReplacer version {0}" -StringValues '0.2.0 (Release)'
 
 # Import Function Parameters
 try{
